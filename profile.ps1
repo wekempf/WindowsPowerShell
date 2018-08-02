f4faf3292bf7c35cc1d34cd6cc5b791838f42696
Bill Kempf <Bill.Kempf@salliemae.com>
2018-08-02 11:47:45 -0400

    Set background to red when elevated

--- profile.ps1
+++ profile.ps1
@@ -1,4 +1,17 @@
-﻿$ProfileDir = Split-Path $profile
+﻿$IsAdmin = & {
+    $wid = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $prp = new-object System.Security.Principal.WindowsPrincipal($wid)
+    $adm = [System.Security.Principal.WindowsBuiltInRole]::Administrator
+    $prp.IsInRole($adm)
+}
+
+if ($IsAdmin) {
+    (get-host).UI.RawUI.Backgroundcolor="DarkRed"
+}
+clear-host
+
+Set-Variable -Name ProfileDir -Value (Split-Path $profile)
+$env:Editor = 'code'
 
 # Add Scripts directories to path
 & "$(Join-Path $PSScriptRoot scripts\Add-Path.ps1)" $(Join-Path $PSScriptRoot scripts)
@@ -10,14 +23,14 @@ if (Test-Path ~\bin) {
     Add-Path ~\bin
 }
 
-# Setup posh-git
+# Load and customize posh-git
 if (Get-Module -Name posh-git -ListAvailable) {
     Import-Module posh-git
     $GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
     $GitPromptSettings.DefaultPromptPath = '$(Get-ShortLocationName)'
 }
 
-# Setup Z
+# Load Z
 if (Get-Module -Name Z -ListAvailable) {
     Import-Module Z
 }
