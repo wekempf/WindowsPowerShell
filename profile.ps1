eddc8695ca3106d4c1cc9176c4cb646217b2e00c
William Kempf <wekempf@outlook.com>
2019-09-20 08:40:52 -0400

    Add oh-my-posh

--- profile.ps1
+++ profile.ps1
@@ -1,6 +1,11 @@
 ﻿# I customize the shell to use 16pt Consolas with a window opacity of 90%
 # If I knew how you could change that programatically I'd automate this bit.
 
+$DefaultUser = 'e79209@B9FG5Q2'
+
+$PSDefaultParameterValues.Add('Invoke-WebRequest:Proxy','https://104.129.194.36:443')
+$PSDefaultParameterValues.Add('Invoke-WebRequest:ProxyUseDefaultCredentials',$true)
+
 # Detect elevation
 $IsAdmin = & {
     $wid = [System.Security.Principal.WindowsIdentity]::GetCurrent()
@@ -33,15 +38,25 @@ if (Test-Path ~\bin) {
 }
 
 # Load and customize posh-git
+# Colors are optimized for a console window with a background color of black
+# and match my cutomized git configuration:
+# [color]
+#     ui = true
+# [color "status"]
+#     changed = yellow bold
+#     untracked = cyan bold
+#     added = green bold
 if (Get-Module -Name posh-git -ListAvailable) {
     Import-Module posh-git
-    $GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
     $GitPromptSettings.DefaultPromptPath = '$(Get-ShortLocationName)'
-    $GitPromptSettings.WorkingForegroundColor='Yellow'
-    $GitPromptSettings.LocalWorkingStatusForegroundColor='Red'
+    $GitPromptSettings.DefaultPromptPath.ForegroundColor = 'Orange'
+    $GitPromptSettings.WorkingColor.ForegroundColor = 'Yellow'
+    $GitPromptSettings.IndexColor.ForegroundColor = 'Lime'
+    $GitPromptSettings.LocalWorkingStatusSymbol = "$([char]27)[1;31m!$([char]27)[0m" # bold red instead of plain red
+    $GitPromptSettings.DefaultPromptSuffix = " $('→' * ($nestedPromptLevel + 1)) "
     if ($IsAdmin) {
-        $GitPromptSettings.DefaultPromptSuffix = "$('$' * ($nestedPromptLevel + 1)) "
-        $GitPromptSettings.DefaultPromptDebugSuffix = " [DBG]$('$' * ($nestedPromptLevel + 1)) "
+        $GitPromptSettings.DefaultPromptPath.ForegroundColor = 'Red'
+        $GitPromptSettings.DefaultPromptSuffix.ForegroundColor = 'Red'
     }
 }
 
@@ -59,3 +74,10 @@ if (Get-Module -Name Z -ListAvailable) {
 # processes. This *may* break some things, so I may need to remove this value in the
 # future?
 # set-itemproperty -path hkcu:\console -name VirtualTerminalLevel -Value 1
+
+Set-Theme wek
+$ThemeSettings.Colors.AdminIconForegroundColor = 'Red'
+$ThemeSettings.Colors.GitForegroundColor = 'White'
+$ThemeSettings.Colors.GitDefaultColor = 'DarkGreen'
+$ThemeSettings.Colors.GitLocalChangesColor = 'DarkMagenta'
+$ThemeSettings.Colors.GetNoLocalChangesAndAheadColor = 'GoldenRod'
